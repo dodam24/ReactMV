ab374b1cc6725876ccb1bf4cac745dd3459a1279
dodam24 <dodam0724@gmail.com>
2023-09-01 15:50:11 +0900

    The MovieDB API

--- boilerplate-mern-stack-master/client/src/components/Config.js
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+// Movie DB (latest, detail, reivew)
+export const API_URL = 'https://api.themoviedb.org/3/';
+
+// Movie 포스터
+export const IMAGE_BASE_URL ='http://image.tmdb.org/t/p/';
